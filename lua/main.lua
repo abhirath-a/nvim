14e4bdfc7d6cde07ac55c9d85169c2709f04bd0a
abhi <85126640+TalkativeDiv@users.noreply.github.com>
2025-06-11 19:32:58 -0400

    changed theme in lazy.nvim

--- lua/main.lua
+++ lua/main.lua
@@ -59,7 +59,7 @@ require("lazy").setup({
 
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
-	install = { colorscheme = { "nvim-tundra" } },
+	install = { colorscheme = { "solarized-osaka" } },
 	-- automatically check for plugin updates
 	checker = {
 		enabled = true, -- check for plugin updates periodically
