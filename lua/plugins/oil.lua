c8b2f7ce70d5247980f76aa25cfcc042971aeb4b
Abhi <85126640+TalkativeDiv@users.noreply.github.com>
2025-07-05 21:58:52 -0400

    added grug-far

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,10 +1,14 @@
-
 return {
   "stevearc/oil.nvim",
   ---@module 'oil'
   ---@type oil.SetupOpts
-  keys = {{ "<C-n>", "<CMD>Oil --float<CR>", desc="open oil file explorer" }},
+  keys = { { "<C-n>", "<CMD>Oil --float<CR>", desc = "open oil file explorer" } },
   opts = {
+    keymaps = {
+      ["<CR>"] = "actions.select",    -- Enter: open file or directory
+      ["<Left>"] = "actions.parent",  -- Left Arrow: go to parent directory
+      ["<Right>"] = "actions.select", -- Right Arrow: open (like Enter)
+    },
     default_file_explorer = true,
     skip_confirm_for_simple_edits = true,
     view_options = {
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,41 +1,41 @@
 return {
-  {
-    "AstroNvim/astrotheme",
-    priority = 10000,
-    lazy = false,
-    config = function()
-      require("astrotheme").setup({
-        palette = "astrodark", -- String of the default palette to use when calling `:colorscheme astrotheme`
-        background = {         -- :h background, palettes to use when using the core vim background colors
-          light = "astrolight",
-          dark = "astrodark",
-        },
-
-        style = {
-          transparent = false,         -- Bool value, toggles transparency.
-          inactive = true,             -- Bool value, toggles inactive window color.
-          float = true,                -- Bool value, toggles floating windows background colors.
-          neotree = true,              -- Bool value, toggles neo-trees background color.
-          border = true,               -- Bool value, toggles borders.
-          title_invert = true,         -- Bool value, swaps text and background colors.
-          italic_comments = true,      -- Bool value, toggles italic comments.
-          simple_syntax_colors = true, -- Bool value, simplifies the amounts of colors used for syntax highlighting.
-        },
-
-
-        termguicolors = true,    -- Bool value, toggles if termguicolors are set by AstroTheme.
-
-        terminal_color = true,   -- Bool value, toggles if terminal_colors are set by AstroTheme.
-
-        plugin_default = "auto", -- Sets how all plugins will be loaded
-        -- "auto": Uses lazy / packer enabled plugins to load highlights.
-        -- true: Enables all plugins highlights.
-        -- false: Disables all plugins.
-
-      })
-      vim.cmd.colorscheme("astrotheme")
-    end
-  }
+  -- {
+  --   "AstroNvim/astrotheme",
+  --   priority = 10000,
+  --   lazy = false,
+  --   config = function()
+  --     require("astrotheme").setup({
+  --       palette = "astrodark", -- String of the default palette to use when calling `:colorscheme astrotheme`
+  --       background = {         -- :h background, palettes to use when using the core vim background colors
+  --         light = "astrolight",
+  --         dark = "astrodark",
+  --       },
+  --
+  --       style = {
+  --         transparent = false,         -- Bool value, toggles transparency.
+  --         inactive = true,             -- Bool value, toggles inactive window color.
+  --         float = true,                -- Bool value, toggles floating windows background colors.
+  --         neotree = true,              -- Bool value, toggles neo-trees background color.
+  --         border = true,               -- Bool value, toggles borders.
+  --         title_invert = true,         -- Bool value, swaps text and background colors.
+  --         italic_comments = true,      -- Bool value, toggles italic comments.
+  --         simple_syntax_colors = true, -- Bool value, simplifies the amounts of colors used for syntax highlighting.
+  --       },
+  --
+  --
+  --       termguicolors = true,    -- Bool value, toggles if termguicolors are set by AstroTheme.
+  --
+  --       terminal_color = true,   -- Bool value, toggles if terminal_colors are set by AstroTheme.
+  --
+  --       plugin_default = "auto", -- Sets how all plugins will be loaded
+  --       -- "auto": Uses lazy / packer enabled plugins to load highlights.
+  --       -- true: Enables all plugins highlights.
+  --       -- false: Disables all plugins.
+  --
+  --     })
+  --     vim.cmd.colorscheme("astrotheme")
+  --   end
+  -- }
   -- {
   --   "navarasu/onedark.nvim",
   --   priority = 1000, -- make sure to load this before all the other start plugins
@@ -47,17 +47,17 @@ return {
   --     require('onedark').load()
   --   end
   -- }
-  --   {
-  --     "tiagovla/tokyodark.nvim",
-  --     opts = {
-  --         -- custom options here
-  --     },
-  --     priority=100000,
-  --     lazy=false,
-  --     config = function(_, opts)
-  --         require("tokyodark").setup(opts) -- calling setup is optional
-  --         vim.cmd [[colorscheme tokyodark]]
-  --     end,
+  -- {
+  --   "tiagovla/tokyodark.nvim",
+  --   opts = {
+  --     -- custom options here
+  --   },
+  --   priority = 100000,
+  --   lazy = false,
+  --   config = function(_, opts)
+  --     require("tokyodark").setup(opts)     -- calling setup is optional
+  --     vim.cmd [[colorscheme tokyodark]]
+  --   end,
   -- }
   -- {
   --   "sainnhe/gruvbox-material",
@@ -99,15 +99,15 @@ return {
   -- 		vim.cmd.colorscheme("catppuccin")
   -- 	end,
   -- },
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {},
-  --   config = function()
-  --     vim.cmd([[colorscheme tokyonight-night]])
-  --   end,
-  -- },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      vim.cmd([[colorscheme tokyonight-night]])
+    end,
+  },
   -- {
   -- 	"rose-pine/neovim",
   -- 	lazy = false,
