266688a49e23f26ec39e86447233973f11b611e9
Abhi <85126640+abhirath-a@users.noreply.github.com>
2025-07-14 06:59:46 -0400

    new approach?

--- flake.nix
+++ flake.nix
@@ -6,47 +6,20 @@
   };
 
   outputs =
-    { self, nixpkgs }:
+    { self, nixpkgs, ... }:
     let
-      system = "x86_64-linux";
+      system = "x86_64-linux"; # or builtins.currentSystem
       pkgs = import nixpkgs { inherit system; };
     in
     {
-      packages.${system}.default = pkgs.stdenv.mkDerivation {
-        pname = "abhivim";
-        version = "0.1";
-        src = ./.;
-        buildInputs = [
-          pkgs.neovim
-          pkgs.nodejs
-          pkgs.ripgrep
-          pkgs.fzf
-          pkgs.fd
-        ];
-
-        # No build phase, just create a launcher script
-        installPhase = ''
-          mkdir -p $out/bin
-          cat > $out/bin/nvim <<EOF
-          #!${pkgs.stdenv.shell}
-          export PATH=${pkgs.nodejs}/bin:${pkgs.ripgrep}/bin:${pkgs.fzf}/bin:${pkgs.fd}/bin:\$PATH
-          exec ${pkgs.neovim}/bin/nvim "\$@"
-          EOF
-          chmod +x $out/bin/nvim
-        '';
-
-        meta = with pkgs.lib; {
-          description = "Abhi's neovim config packaged as nix flake";
-          license = licenses.mit;
-          maintainers = with maintainers; [ ];
-        
-	platforms = platforms.all;
-};
-      };
-
-      apps.${system}.default = {
-        type = "app";
-        program = "${self.packages.${system}.default}/bin/nvim";
-      };
+      packages.${system}.default =
+        pkgs.runCommand "nvim-config"
+          {
+            # no build inputs because just copying files
+          }
+          ''
+            mkdir -p $out/config/nvim
+            cp ${self}/init.lua $out/config/nvim/init.lua
+          '';
     };
 }
