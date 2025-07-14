9e3731cda57c0e3275dba433d4e9dbb02389a920
Abhi <85126640+abhirath-a@users.noreply.github.com>
2025-07-14 07:02:30 -0400

    another new approach

--- flake.nix
+++ flake.nix
@@ -1,25 +1,15 @@
 {
   description = "Abhi's neovim config packaged as nix flake";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+#--  inputs = {
+ # --  nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+# -- };
+  outputs = { self, ... }:
+  {
+    # Just expose the source directory so others can use your config files
+    defaultPackage.x86_64-linux = self;
 
-  outputs =
-    { self, nixpkgs, ... }:
-    let
-      system = "x86_64-linux"; # or builtins.currentSystem
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      packages.${system}.default =
-        pkgs.runCommand "nvim-config"
-          {
-            # no build inputs because just copying files
-          }
-          ''
-            mkdir -p $out/config/nvim
-            cp ${self}/init.lua $out/config/nvim/init.lua
-          '';
-    };
+    # Optional devShell if you want
+    devShell.x86_64-linux = import ./shell.nix; # optional
+  };
 }
