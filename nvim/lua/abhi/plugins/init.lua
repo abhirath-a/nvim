8dfbf1545d0e247cc78eb054112d9ed1b5e491af
Abhi <85126640+abhirath-a@users.noreply.github.com>
2025-08-10 22:17:42 -0400

    fix: made fidget transparent

--- nvim/lua/abhi/plugins/init.lua
+++ nvim/lua/abhi/plugins/init.lua
@@ -31,6 +31,9 @@ return {
       require("fidget").setup({
         notification = {
           override_vim_notify = true,
+          window = {
+            winblend = 0
+          }
         },
       })
     end,
